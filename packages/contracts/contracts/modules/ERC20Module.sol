f306326d1fbb1d5fc21e5546d8c4bd06f84f3bc5
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-10-10 19:11:42 +0700

    fix: use SafeERC20 for transfer

--- packages/contracts/contracts/modules/ERC20Module.sol
+++ packages/contracts/contracts/modules/ERC20Module.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../interfaces/IERC6551Executable.sol";
 
 import "../interfaces/IPackModule.sol";
@@ -38,7 +39,8 @@ contract ERC20Module is IPackModule {
         for (uint256 i = 0; i < tokensData.length; i++) {
             OnCreateData memory tokenData = tokensData[i];
 
-            IERC20(tokenData.tokenAddress).transferFrom(
+            SafeERC20.safeTransferFrom(
+                IERC20(tokenData.tokenAddress),
                 msg.sender,
                 account,
                 tokenData.amount
