a78d6dd4c837f8c534a5f0111936acdb00ca5c88
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-10-10 21:59:47 +0700

    fix: accounts for testing using mnemonic

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,28 +1,27 @@
 import { config as dotenvConfig } from "dotenv";
 dotenvConfig({ path: __dirname + "/.env" });
 
-import './tasks'
+import "./tasks";
 
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-
-const accounts = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+const accounts =
+  process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     hardhat: {
       chainId: 31337,
-      accounts: [{
-        privateKey: accounts[0],
-        balance: '10000000000000000000'
-      }]
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
     },
     // Faucet RPC, etc : https://docs.scroll.io/en/developers/developer-quickstart/#hardhat
     scrollSepolia: {
       url: "https://sepolia-rpc.scroll.io/" || "",
-      accounts: accounts
+      accounts: accounts,
     },
 
     //  https://faucet.polygon.technology/
@@ -34,7 +33,7 @@ const config: HardhatUserConfig = {
     // Faucets, RPC, etc: https://windranger-io.notion.site/Developer-Starter-Guide-9e9de7a4e60a49dc97dd786c48ffd455
     mantleTestnet: {
       url: "https://rpc.testnet.mantle.xyz/" || "",
-      accounts: accounts
+      accounts: accounts,
     },
   },
   etherscan: {
