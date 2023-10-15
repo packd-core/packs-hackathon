2185e5fe2a7ea7453550f21f31b5c1f2bf0b4af5
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-15 17:31:40 +0200

    fix: help item padding

--- packages/app/app/components/content/HelpItem.tsx
+++ packages/app/app/components/content/HelpItem.tsx
@@ -11,7 +11,7 @@ export const HelpItem = ({ title, children, className }: HelpItemProps ) => {
         <span className='text-card-title flex items-center text-sm'>
             <Help className='h-4 w-4 inline mr-2'/> {title}
             </span>
-        <div className='p-2 rounded-lg text-gray-300 text-sm'>
+        <div className='rounded-lg text-gray-300 text-sm'>
             {children}
         </div>
     </ContentCard>
