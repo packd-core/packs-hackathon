d226424197f277b8fb8f8e37c8b5ab5ad8f1fa35
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-16 19:41:48 +0200

    feat: add loading card

---
+++ packages/app/app/components/content/LoadingCard.tsx
@@ -0,0 +1,35 @@
+import Present from "~/present.svg";
+import {ImNewTab} from "react-icons/im";
+import {Card} from "@/app/components/Card";
+import clsxm from "@/src/lib/clsxm";
+
+type LoadingCardProps = {
+    className?: string;
+    children?: React.ReactNode;
+    title?: string;
+    text?: string;
+    transactionHash?: string;
+}
+
+export function LoadingCard({className, children, title, text, transactionHash}: LoadingCardProps) {
+    return <Card
+        className={clsxm('mx-auto w-full bg-gray-800', className)}
+        containerClassName='max-h-[60vh] overflow-y-auto'
+        controls={
+            <div className='w-full flex justify-center py-1'>
+                {text}
+            </div>}>
+        <div className="flex flex-col items-center gap-2">
+            <div className="p-2 rounded-full bg-gray-800">
+                <Present className={'h-6 w-6'}/>
+            </div>
+            <h1 className="text-lg pt-4">{title}</h1>
+            {
+                transactionHash &&
+                <a href={'https://etherscan.io'} target={'_blank'} className='text-primary-500'>Track it
+                    onchain <ImNewTab className="inline"/></a>
+            }
+            {children}
+        </div>
+    </Card>
+}
