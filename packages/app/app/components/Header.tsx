93a543b2c8813b75c68a7bc0bbd0f4809dd70f51
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-16 21:11:02 +0200

    feat: add on scroll effects to header

--- packages/app/app/components/Header.tsx
+++ packages/app/app/components/Header.tsx
@@ -1,31 +1,52 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Wrapper } from "./Wrapper";
+'use client'
+import {ConnectButton} from "@rainbow-me/rainbowkit";
+import {Wrapper} from "./Wrapper";
 import {PackdLogo} from "@/app/components/header/PackdLogo";
 import {CiTwitter} from "react-icons/ci";
 import {AiFillTwitterSquare} from "react-icons/ai";
 import {BsDisc, BsDiscord, BsGithub, BsTwitter} from "react-icons/bs";
+import {useEffect, useRef, useState} from "react";
+import clsxm from "@/src/lib/clsxm";
 
 const Header = () => {
-  return (
-    <header className="py-4 mb-4 sm:py-8 sm:mb-10 border-b  fixed top-0 left-0 right-0">
-      <Wrapper>
-        <div className="flex items-center justify-between">
-          <PackdLogo />
-          <div className='flex h-full items-center gap-2'>
-              <BsTwitter className='text-white text-2xl hidden sm:flex'/>
-              <BsDiscord className='text-white text-2xl hidden sm:flex'/>
-              <BsGithub className='text-white text-2xl hidden sm:flex'/>
-            <div className='w-[1px] bg-white/20 h-[34px]'></div>
-            <ConnectButton
-                showBalance={false}
-                accountStatus="address"
-                label="Connect"
-            />
-          </div>
-        </div>
-      </Wrapper>
-    </header>
-  );
+    const [scrolled, setScrolled] = useState(false);
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            console.log(entry.isIntersecting)
+            setScrolled(!entry.isIntersecting)
+        });
+        const trigger = document.querySelector('[data-header-trigger]');
+        if (trigger) {
+            observer.observe(trigger);
+        }
+        return () => {
+            observer.disconnect();
+        }
+    }, []);
+    return (
+        <header
+            className={clsxm(
+                'z-20 py-4 mb-4 sm:py-8 sm:mb-10 border-b sticky top-0 left-0 right-0',
+                'transition duration-200',
+                scrolled && 'backdrop-blur-md bg-black/10')}>
+            <Wrapper>
+                <div className="flex items-center justify-between">
+                    <PackdLogo/>
+                    <div className='flex h-full items-center gap-2'>
+                        <BsTwitter className='text-white text-2xl hidden sm:flex'/>
+                        <BsDiscord className='text-white text-2xl hidden sm:flex'/>
+                        <BsGithub className='text-white text-2xl hidden sm:flex'/>
+                        <div className='w-[1px] bg-white/20 h-[34px]'></div>
+                        <ConnectButton
+                            showBalance={false}
+                            accountStatus="address"
+                            label="Connect"
+                        />
+                    </div>
+                </div>
+            </Wrapper>
+        </header>
+    );
 };
 
-export { Header };
+export {Header};
--- packages/app/app/layout.tsx
+++ packages/app/app/layout.tsx
@@ -1,37 +1,37 @@
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import "react-toastify/dist/ReactToastify.css";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import {Manrope} from "next/font/google";
-import { Providers } from "./components/Providers";
-import { Header } from "./components/Header";
-import { ToastContainer } from "react-toastify";
+import {Providers} from "./components/Providers";
+import {Header} from "./components/Header";
+import {ToastContainer} from "react-toastify";
 import clsxm from "@/src/lib/clsxm";
 
-const manrope = Manrope({ subsets: ["latin"] });
+const manrope = Manrope({subsets: ["latin"]});
 export const metadata: Metadata = {
-  title: "Solidity Next.js Starter",
-  description:
-    "A starter kit for building full stack Ethereum dApps with Solidity and Next.js",
+    title: "Solidity Next.js Starter",
+    description:
+        "A starter kit for building full stack Ethereum dApps with Solidity and Next.js",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={clsxm(
-          manrope.className,
-          'bg-gradient-to-b from-[#F15025] via-[rgba(177,194,218,0.25)] to-[#B1C2DA] min-h-screen'
-      )}>
+    return (
+        <html lang="en">
+        <body className={clsxm(manrope.className, 'relative')}>
+        <div
+            className='-z-10 fixed inset-0 bg-gradient-to-b from-[#F15025] via-[rgba(177,194,218,0.25)] to-[#B1C2DA] min-h-screen'></div>
         <Providers>
-          <Header />
-          {children}
+            <div data-header-trigger></div>
+            <Header/>
+            {children}
         </Providers>
-        <ToastContainer />
-      </body>
-    </html>
-  );
+        <ToastContainer/>
+        </body>
+        </html>
+    );
 }
