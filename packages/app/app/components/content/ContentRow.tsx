d2c0d5f3250fee323521a752383bd4ae503f91c6
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-16 19:25:55 +0200

    feat: add ContentTitle

--- packages/app/app/components/content/ContentRow.tsx
+++ packages/app/app/components/content/ContentRow.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from "react";
+import clsxm from "@/src/lib/clsxm";
 
 type ContentRowProps = {
     label: ReactNode;
@@ -9,5 +10,22 @@ export function ContentRow({label, value}: ContentRowProps) {
     return <div className="bg-gray-800 rounded-full flex justify-between px-2 py-1">
         <span className="flex justify-center items-center font-semibold text-gray-500">{label}</span>
         <span className="flex justify-center items-center font-semibold text-white">{value}</span>
+    </div>;
+}
+
+type ContentTitleProps = {
+    children: ReactNode;
+    className?: string;
+    borderBottom?: boolean;
+    borderTop?: boolean;
+}
+
+export function ContentTitle({children, className, borderBottom = true, borderTop = false}: ContentTitleProps) {
+    return <div className={clsxm("self-stretch border-gray-500 text-sm py-2",
+        {
+            'border-b-[1px]': borderBottom,
+            'border-t-[1px]': borderTop,
+        }, className)}>
+        {children}
     </div>;
 }
