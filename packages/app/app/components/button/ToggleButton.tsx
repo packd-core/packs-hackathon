be6db84ce8577b450685ed1d522040b3305d73e4
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-17 22:13:27 +0200

    feat: add ToggleButton.tsx

---
+++ packages/app/app/components/button/ToggleButton.tsx
@@ -0,0 +1,10 @@
+import {ReactNode} from "react";
+import clsxm from "@/src/lib/clsxm";
+
+export function ToggleButton(props: { selected: boolean, onClick: () => void, children: ReactNode }) {
+    return <button className={clsxm(
+        'text-white rounded px-4 py-1',
+        'hover:text-gray-100 hover:bg-black/30'
+        , props.selected && "bg-black text-primary-500 hover:text-primary-500/80 hover:bg-black/80")}
+                   onClick={props.onClick}> {props.children}</button>;
+}
