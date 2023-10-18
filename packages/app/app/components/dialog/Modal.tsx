22d3a8f2f3d69537973469ee8cbc856464fdab39
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-18 19:38:08 +0200

    feat: add general Modal

---
+++ packages/app/app/components/dialog/Modal.tsx
@@ -0,0 +1,53 @@
+import {Dialog, Transition} from '@headlessui/react'
+import {Fragment, ReactNode} from 'react'
+
+export default function Modal({render, isOpen, setIsOpen}: { render: (closeModal: () => void) => ReactNode, isOpen: boolean, setIsOpen: (isOpen: boolean) => void }) {
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    return (
+
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => {
+            }}>
+                <div className="fixed inset-0 bg-black/30" aria-hidden="true"/>
+
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25"/>
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel
+                                className="w-full max-w-md transform overflow-hidden p-0 bg-transparent align-middle transition-all">
+                                {render(closeModal)}
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    )
+}
