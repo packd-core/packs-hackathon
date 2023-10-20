b037a5708f69d6bec20e56e137302468793868ca
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-20 18:00:50 +0200

    fix: remove unused imports and fix the text

--- packages/app/app/mint/pack/SignForm.tsx
+++ packages/app/app/mint/pack/SignForm.tsx
@@ -1,5 +1,3 @@
-import {ContentCard} from "@/app/components/content/ContentCard";
-import Button from "@/app/components/button/Button";
 import {HelpItem} from "@/app/components/content/HelpItem";
 
 export const SignForm = () => (
@@ -9,6 +7,6 @@ export const SignForm = () => (
             <p className='text-sm text-gray-500'>Sign the Message to generate a claim key.</p>
         </div>
         <HelpItem title={'What am I signing?'}>
-            The signature is used as a seed for creating a new claim key. The singature content is determined by the pack information, so you can reconstruct a claim key in the future again.
+            The signature is used as a seed for creating a new claim key. The signature content is determined by the pack information, so you can reconstruct a claim key in the future again.
         </HelpItem>
     </div>)
