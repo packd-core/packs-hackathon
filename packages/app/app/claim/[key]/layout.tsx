0169b8adab1ebb767f54364258401c757294c607
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-22 00:25:38 +0200

    feat: reset useClaimState in layout

---
+++ packages/app/app/claim/[key]/layout.tsx
@@ -0,0 +1,24 @@
+'use client'
+import {Wrapper} from "@/app/components/Wrapper";
+import {ReactNode, useEffect} from "react";
+import {usePackState} from "@/app/mint/usePackState";
+import {useClaimState} from "@/app/claim/[key]/useClaimState";
+
+
+export default function ClaimLayout({children}: {
+    children: ReactNode;
+}) {
+    const resetStepper = useClaimState(state => state.reset);
+    useEffect(() => {
+        resetStepper();
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <main>
+            <Wrapper className='flex items-center mb-12'>
+                {children}
+            </Wrapper>
+        </main>
+    );
+}
