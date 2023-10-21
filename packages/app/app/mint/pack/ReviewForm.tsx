58c53f3c597ac761b0e7b07188396fcfdf5ea7ef
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-21 18:46:29 +0200

    feat: add eth to review form

--- packages/app/app/mint/pack/ReviewForm.tsx
+++ packages/app/app/mint/pack/ReviewForm.tsx
@@ -4,12 +4,14 @@ import Button from "@/app/components/button/Button";
 import {FiArrowLeft, FiArrowRight} from "react-icons/fi";
 import {usePackState} from "@/app/mint/usePackState";
 import usePackdAddresses from "@/src/hooks/usePackdAddresses";
-import { useNetwork} from "wagmi";
+import {useNetwork} from "wagmi";
 import {useMintStore} from "@/src/stores/useMintStore";
 import Erc721Card from "@/app/mint/modules/Erc721Module";
 import Erc20Card from "@/app/mint/modules/Erc20Module";
 import {ContentCard} from "@/app/components/content/ContentCard";
 import useMintPack from "@/src/hooks/useMintPack";
+import {BsX} from "react-icons/bs";
+import {formatEther} from "ethers";
 
 
 export const ReviewForm = () => {
@@ -19,8 +21,9 @@ export const ReviewForm = () => {
     const setControls = usePackState(state => state.setControls)
     const addresses = usePackdAddresses();
     const modules = useMintStore(state => state.modules)
-    const { chain} = useNetwork();
-    const {write, isLoading,error, data} = useMintPack();
+    const amountEth = useMintStore(state => state.eth)
+    const {chain} = useNetwork();
+    const {write, isLoading, error, data} = useMintPack();
     useEffect(() => {
         if (data?.hash) {
             setHash(data!.hash)
@@ -47,6 +50,13 @@ export const ReviewForm = () => {
                 <h2 className="text-2xl font-bold ">Review Pack Content</h2>
             </div>
             <ContentTitle>Contents</ContentTitle>
+            <ContentCard className="self-stretch">
+                <div className="flex justify-between">
+                    <span className="text-card-title">Eth</span>
+                </div>
+                <input className="text-right w-full " disabled={true}
+                       value={formatEther(amountEth ?? 0)}/>
+            </ContentCard>
             {modules.map((module, index) => {
                 if (module.moduleAddress === addresses.ERC721Module) {
                     return <Erc721Card key={module.address + module.value}
