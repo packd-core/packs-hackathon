8d5c977057bcf5d9745ae398139ab091c4dfba4e
Adam Kolozsvari <kolozsvariadam@gmail.com>
2023-10-21 16:43:52 +0200

    feat: reset stores in mint/layout

--- packages/app/app/mint/layout.tsx
+++ packages/app/app/mint/layout.tsx
@@ -1,16 +1,26 @@
+"use client"
 import type {Metadata} from "next";
 import {Wrapper} from "@/app/components/Wrapper";
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
+import {useMintStore} from "@/src/stores/useMintStore";
+import {usePackState} from "@/app/mint/usePackState";
 
-export const metadata: Metadata = {
-    title: "Create Pack - PACKD",
-    description:
-        "A starter kit for building full stack Ethereum dApps with Solidity and Next.js",
-};
+// export const metadata: Metadata = {
+//     title: "Create Pack - PACKD",
+//     description:
+//         "A starter kit for building full stack Ethereum dApps with Solidity and Next.js",
+// };
 
 export default function MintLayout({children}: {
     children: ReactNode;
 }) {
+    const resetMint = useMintStore(state => state.reset);
+    const resetStepper = usePackState(state => state.reset);
+    useEffect(() => {
+        resetMint();
+        resetStepper();
+        // eslint-disable-next-line
+    }, []);
     return (
         <main>
             <Wrapper className='flex items-center mb-12'>
