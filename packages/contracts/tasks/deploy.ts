1ff1fc614a7466878fbf4bfa0b2fae1345347994
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-10-22 15:28:14 +0700

    feat: get address deployed on mint

--- packages/contracts/tasks/deploy.ts
+++ packages/contracts/tasks/deploy.ts
@@ -2,6 +2,7 @@ import { subtask, task } from "hardhat/config";
 import { deploySystem } from "../scripts/deploy";
 import { getSystemConfig } from "../utils/deployConfig";
 import { logger } from "../utils/deployUtils";
+import { getDeployedAddress } from "../utils/saveAddress";
 const info = logger("info", "task");
 
 subtask(
@@ -29,10 +30,25 @@ task(
   await hre.run("deploy", args);
   // Setup 3  test accounts, dao, alice, bob
   // Pre compute address with default deployer
-  // erc20MockA.target: 0x7D0B2154C5c709b3Cc8489286e023Cf75a38E0B5
-  // erc20MockB.target: 0x416D29fbCf9fc5CA66d792B1f6368221E985ec47
-  // erc721MockA.target: 0x0106f5483Ace34618dCC1c76EFF7e284e5dE4C6B
-  // erc721MockB.target: 0x958B411CB2cf43678ca9d366a8a6469CEa85B8fE
+
+  const isLocal =
+    hre.network.name === "hardhat" || hre.network.name === "localhost";
+
+  let addresses = {
+    erc20MockA: isLocal
+      ? "0x7D0B2154C5c709b3Cc8489286e023Cf75a38E0B5"
+      : await getDeployedAddress(hre, "ERC20MockA"),
+    erc20MockB: isLocal
+      ? "0x416D29fbCf9fc5CA66d792B1f6368221E985ec47"
+      : await getDeployedAddress(hre, "ERC20MockB"),
+    erc721MockA: isLocal
+      ? "0x0106f5483Ace34618dCC1c76EFF7e284e5dE4C6B"
+      : await getDeployedAddress(hre, "ERC721MockA"),
+    erc721MockB: isLocal
+      ? "0x958B411CB2cf43678ca9d366a8a6469CEa85B8fE"
+      : await getDeployedAddress(hre, "ERC721MockB"),
+  };
+
   let tokenId = 0;
   for (let i = 1; i <= 3; i++) {
     const account = process.env[`ACCOUNT_${i}`];
@@ -40,23 +56,23 @@ task(
       await hre.run("send:eth", { account: account, amount: 1 });
       await hre.run("mint:erc20", {
         account: account,
-        tokenaddress: "0x7D0B2154C5c709b3Cc8489286e023Cf75a38E0B5",
+        tokenaddress: addresses.erc20MockA,
         amount: 1000,
       });
       await hre.run("mint:erc20", {
         account: account,
-        tokenaddress: "0x416D29fbCf9fc5CA66d792B1f6368221E985ec47",
+        tokenaddress: addresses.erc20MockB,
         amount: 1000,
       });
 
       await hre.run("mint:erc721", {
         account: account,
-        tokenaddress: "0x0106f5483Ace34618dCC1c76EFF7e284e5dE4C6B",
+        tokenaddress: addresses.erc721MockA,
         tokenid: tokenId++,
       });
       await hre.run("mint:erc721", {
         account: account,
-        tokenaddress: "0x958B411CB2cf43678ca9d366a8a6469CEa85B8fE",
+        tokenaddress: addresses.erc721MockB,
         tokenid: tokenId++,
       });
     }
