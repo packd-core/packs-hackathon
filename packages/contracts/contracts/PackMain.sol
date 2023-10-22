ad663cf0360ae56de3874c0f1c2ece8614c97e79
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-10-22 16:17:44 +0700

    chore: add natspec

--- packages/contracts/contracts/PackMain.sol
+++ packages/contracts/contracts/PackMain.sol
@@ -14,15 +14,28 @@ import "./PackNFT.sol";
 import "./ClaimData.sol";
 import "./lib/SignatureValidator.sol";
 
+/**
+ * @title PackMain
+ * @dev This is the primary contract for the Pack protocol. It handles the creation, management, and execution of packs.
+ */
 contract PackMain is PackNFT, Ownable {
     // ---------- Events ---------------------
+    /**
+     * @dev Emitted when a new pack is created
+     */
     event PackCreated(
         uint256 indexed tokenId,
         address owner,
         address[] modules,
         bytes[] moduleData
     );
+    /**
+     * @dev Emitted when a pack is revoked
+     */
     event PackRevoked(uint256 indexed tokenId, address owner);
+    /**
+     * @dev Emitted when a pack is opened
+     */
     event PackOpened(uint256 indexed tokenId, address claimer);
 
     // ---------- Errors ---------------------
@@ -51,6 +64,18 @@ contract PackMain is PackNFT, Ownable {
     // Modules whitelist
     mapping(address => bool) public modulesWhitelist;
 
+    /**
+     * @dev Initializes the PackMain contract
+     * @param initialOwner_ The address that will be set as the initial owner of the contract
+     * @param baseTokenURI_ The base URI that will be used for all token URIs
+     * @param name_ The name of the NFT token
+     * @param symbol_ The symbol of the NFT token
+     * @param registry_ ERC6551 registry contract address
+     * @param implementation_ ERC6551 implementation contract address
+     * @param registryChainId_ The chain ID of the network, to prevent cross-chain replay attacks
+     * @param salt_ A random value used to ensure the uniqueness of the contract's address
+     * @param modulesWhitelist_ An array of addresses that will be initially whitelisted as valid modules
+     */
     constructor(
         address initialOwner_,
         string memory baseTokenURI_,
@@ -67,6 +92,7 @@ contract PackMain is PackNFT, Ownable {
             revert InvalidAddress();
         }
 
+        // Set ERC6551-related params
         registry = IERC6551Registry(registry_);
         implementation = implementation_;
         registryChainId = registryChainId_;
@@ -76,6 +102,10 @@ contract PackMain is PackNFT, Ownable {
         setModulesWhitelist(modulesWhitelist_, true);
     }
 
+    /**
+     * @dev Modifier to check if the sender is the owner of the token
+     * @param tokenId The ID of the token to check ownership of
+     */
     modifier onlyOwnerOf(uint256 tokenId) {
         if (ownerOf(tokenId) != msg.sender) {
             revert OnlyOwnerOf(tokenId);
@@ -83,6 +113,11 @@ contract PackMain is PackNFT, Ownable {
         _;
     }
 
+    /**
+     * @dev Modifier to check if the token is in the desired state
+     * @param tokenId The ID of the token to check the state of
+     * @param desiredState Struct, the desired state of the token
+     */
     modifier tokenInState(uint256 tokenId, PackState desiredState) {
         if (packState[tokenId] != desiredState) {
             revert TokenNotInExpectedState(tokenId);
@@ -90,6 +125,17 @@ contract PackMain is PackNFT, Ownable {
         _;
     }
 
+    /**
+     * @dev Function to pack a new NFT. This function is responsible for creating a new NFT and assigning it to the owner.
+     * This function also creates a new account for the NFT and transfers some ETH to the new account.
+     * Finally, it loops through the modules and executes the data.
+     * @param to_ The address to which the new NFT will be assigned.
+     * @param claimPublicKey_ The public key that will be associated with the claim of the new NFT.
+     * @param modules The modules that will be associated with the new NFT. These modules must be whitelisted.
+     * @param moduleData The data that the modules will use to execute their logic.
+     * @notice The ETH sent with this function will be transferred to the new account.
+     * @notice The modules and moduleData arrays must be the same length.
+     */
     function pack(
         address to_,
         address claimPublicKey_,
@@ -150,6 +196,12 @@ contract PackMain is PackNFT, Ownable {
         emit PackCreated(tokenId, to_, modules, moduleData);
     }
 
+    /**
+     * @dev This function revokes a pack. It is only callable by the owner of the pack and only if the pack is in the 'Created' state.
+     * @param tokenId_ The unique identifier of the pack to be revoked.
+     * @param moduleData The data associated with the pack's modules.
+     * @notice The moduleData array must be the same length as the modules array.
+     */
     function revoke(
         uint256 tokenId_,
         bytes[] calldata moduleData
@@ -185,6 +237,13 @@ contract PackMain is PackNFT, Ownable {
         emit PackRevoked(tokenId_, msg.sender);
     }
 
+    /**
+     * @dev Function to open a pack
+     * @param data The data associated with the pack to be opened.
+     * @param moduleData The data associated with the pack's modules.
+     * @notice The moduleData array must be the same length as the modules array.
+     * @notice This is normally sent by a relayer, so the relayer will be refunded the refundValue.
+     */
     function open(
         ClaimData memory data,
         bytes[] calldata moduleData
@@ -219,6 +278,12 @@ contract PackMain is PackNFT, Ownable {
         emit PackOpened(data.tokenId, msg.sender);
     }
 
+    /**
+     * @dev Function to set the modules whitelist
+     * @param modules An array of addresses representing the modules to be whitelisted
+     * @param value A boolean value indicating whether the modules should be whitelisted (true) or not (false)
+     * @notice This function can only be called by the owner of the contract
+     */
     function setModulesWhitelist(
         address[] memory modules,
         bool value
@@ -228,6 +293,11 @@ contract PackMain is PackNFT, Ownable {
         }
     }
 
+    /**
+     * @dev This function returns the account associated with a specific token.
+     * @param tokenId The unique identifier of the token whose associated account is to be returned.
+     * @return Returns the address of the account associated with the given tokenId.
+     */
     function account(uint256 tokenId) public view returns (address) {
         return
             registry.account(
@@ -239,6 +309,10 @@ contract PackMain is PackNFT, Ownable {
             );
     }
 
+    /**
+     * @dev This is an internal function that handles the transfer of ETH from the account to the owner and refunds the relayer.
+     * @param data This is a memory structure that contains all the necessary data for the transfer and refund operation.
+     */
     function _transferAndRefund(ClaimData memory data) internal {
         // Refund the relayer
         address payable thisAccount = payable(account(data.tokenId));
@@ -248,6 +322,12 @@ contract PackMain is PackNFT, Ownable {
         _executeTransfer(thisAccount, data.claimer, value);
     }
 
+    /**
+     * @dev This is an internal function that is used to execute a transfer operation.
+     * @param accountAddress The address of the account from which the transfer will be made.
+     * @param recipient The address of the recipient who will receive the transfer.
+     * @param value The amount of ETH that will be transferred.
+     */
     function _executeTransfer(
         address payable accountAddress,
         address recipient,
@@ -267,6 +347,11 @@ contract PackMain is PackNFT, Ownable {
         );
     }
 
+    /**
+     * @dev This is an internal function that validates the signatures associated with a claim.
+     * It uses the SignatureValidator to check the signatures against the claim data, registry chain ID, salt, contract address, and public key associated with the token ID.
+     * @param data The claim data to validate.
+     */
     function _validateSignatures(ClaimData memory data) internal view {
         SignatureValidator.validateSignatures(
             data,
--- packages/contracts/contracts/PackNFT.sol
+++ packages/contracts/contracts/PackNFT.sol
@@ -4,15 +4,25 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+/**
+ * @title PackNFT
+ * @dev This contract inherits from ERC721 and ERC721Enumerable. It represents a Pack NFT and includes functions for minting, revoking and opening packs.
+ */
 contract PackNFT is ERC721, ERC721Enumerable {
     //
     // ----------- Enumerations ---------------
-    // Represents the state of a Pack
+    /**
+     * @dev Represents the state of a Pack
+     * Empty: Indicates that the pack has not been minted yet
+     * Created: Indicates that the pack is newly minted and not yet opened or revoked
+     * Opened: Indicates that the pack has been opened by a claimer
+     * Revoked: Indicates that the pack has been revoked by the owner
+     */
     enum PackState {
-        Empty, // Indicates that the pack has not been minted yet
-        Created, // Indicates that the pack is newly minted and not yet opened or revoked
-        Opened, // Indicates that the pack has been opened by a claimer
-        Revoked // Indicates that the pack has been revoked by the owner
+        Empty,
+        Created,
+        Opened,
+        Revoked
     }
 
     // ---------- Storage --------------------
@@ -25,6 +35,12 @@ contract PackNFT is ERC721, ERC721Enumerable {
     // Store creation block number for each token
     mapping(uint256 => uint256) public creationBlock;
 
+    /**
+     * @dev Constructor for the PackNFT contract
+     * @param baseTokenURI_ The base URI for the token
+     * @param name_ The name of the token
+     * @param symbol_ The symbol of the token
+     */
     constructor(
         string memory baseTokenURI_,
         string memory name_,
@@ -38,7 +54,11 @@ contract PackNFT is ERC721, ERC721Enumerable {
         packStateURIs[uint(PackState.Revoked)] = "revoked";
     }
 
-    // overwrite the _safeMint function to set the state of the Pack
+    /**
+     * @dev Internal function to mint a pack and set its state to Created
+     * @param to The address to mint the pack to
+     * @return tokenId The ID of the newly minted pack
+     */
     function _mintPack(address to) internal returns (uint256 tokenId) {
         tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
@@ -50,6 +70,10 @@ contract PackNFT is ERC721, ERC721Enumerable {
         creationBlock[tokenId] = block.number;
     }
 
+    /**
+     * @dev Internal function to revoke a pack and set its state to Revoked
+     * @param tokenId The ID of the pack to revoke
+     */
     function _revokePack(uint256 tokenId) internal {
         _burn(tokenId);
 
@@ -57,11 +81,20 @@ contract PackNFT is ERC721, ERC721Enumerable {
         packState[tokenId] = PackState.Revoked;
     }
 
+    /**
+     * @dev Internal function to open a pack and set its state to Opened
+     * @param tokenId The ID of the pack to open
+     */
     function _openPack(uint256 tokenId) internal {
         // Set state to Opened
         packState[tokenId] = PackState.Opened;
     }
 
+    /**
+     * @dev Function to get the URI of a token based on its state
+     * @param tokenId The ID of the token
+     * @return The URI of the token
+     */
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
@@ -76,12 +109,23 @@ contract PackNFT is ERC721, ERC721Enumerable {
         return string(abi.encodePacked(_baseURI(), stateURI));
     }
 
+    /**
+     * @dev Function to get the base URI of the token
+     * @return The base URI of the token
+     */
     function _baseURI() internal view override returns (string memory) {
         return _baseTokenURI;
     }
 
     // The following functions are overrides required by Solidity.
 
+    /**
+     * @dev Internal function to update the state of a token
+     * @param to The address to update the token to
+     * @param tokenId The ID of the token
+     * @param auth The address authorized to update the token
+     * @return The address of the updated token
+     */
     function _update(
         address to,
         uint256 tokenId,
@@ -90,6 +134,11 @@ contract PackNFT is ERC721, ERC721Enumerable {
         return super._update(to, tokenId, auth);
     }
 
+    /**
+     * @dev Internal function to increase the balance of an account
+     * @param account The address of the account
+     * @param value The amount to increase the balance by
+     */
     function _increaseBalance(
         address account,
         uint128 value
@@ -97,6 +146,11 @@ contract PackNFT is ERC721, ERC721Enumerable {
         super._increaseBalance(account, value);
     }
 
+    /**
+     * @dev Function to check if the contract supports an interface
+     * @param interfaceId The ID of the interface
+     * @return A boolean indicating whether the contract supports the interface
+     */
     function supportsInterface(
         bytes4 interfaceId
     ) public view override(ERC721, ERC721Enumerable) returns (bool) {
