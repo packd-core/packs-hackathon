9288fc5fe64d9cd4d57aae96270787bb46de1410
kodam <kolozsvariadam@gmail.com>
2023-10-25 22:30:29 +0200

    feat: add new token input with token selector

---
+++ packages/app/app/components/web3/TokenSelectorDialog.tsx
@@ -0,0 +1,159 @@
+import {useEffect, useMemo, useState} from 'react'
+import usePackdAddresses from "@/src/hooks/usePackdAddresses";
+import {ContentCard} from "@/app/components/content/ContentCard";
+import {BsChevronDown, BsSearch, BsX} from "react-icons/bs";
+import {Address, useAccount, useBalance, useToken} from "wagmi";
+import Modal from "@/app/components/dialog/Modal";
+import Icon from '~/chain.svg'
+import {isAddress} from "viem";
+import {formatUnits, parseUnits} from "ethers";
+import clsxm from "@/src/lib/clsxm";
+
+export default function TokenInput({token, value, onTokenSelected, onValueChanged}: {
+    token?: Address,
+    value?: bigint,
+    onTokenSelected?: (address: Address) => void,
+    onValueChanged?: (value: bigint) => void
+}) {
+    const {address} = useAccount()
+    const [isOpen, setIsOpen] = useState(false);
+    const {data: tokenData} = useToken({address: token, enabled: !!token})
+    const {data: balance, isLoading} = useBalance({
+        address: address as Address,
+        token: token as Address,
+        enabled: !!token
+    })
+    const isValidAmount = useMemo(() => {
+        if (!onValueChanged) {
+            return true;
+        }
+       return value && value > 0 && (value <= (balance?.value ?? BigInt(0)));
+    }, [value, balance?.value, onValueChanged]);
+
+    return <div>
+        <TokenSelectorDialog isOpen={isOpen} setIsOpen={setIsOpen} onAdd={(address, name) => {
+            setIsOpen(false);
+            onTokenSelected?.(address);
+            if (address != token) {
+                onValueChanged?.(0n)
+            }
+        }}/>
+        <div className='relative w-full'>
+            <button onClick={() => onValueChanged && setIsOpen(true)}
+                    className='absolute pl-2 left-0 bottom-0 top-0 flex items-center justify-center text-sm font-semibold'>
+                {token ? <><Icon className="mr-1 h-4 shrink-0"/> {tokenData?.name}</> : 'Select token'}
+                {onTokenSelected && <BsChevronDown
+                    className='text-base ml-1 shrink-0'/>}
+            </button>
+
+            <input
+                placeholder="amount"
+                disabled={!token || !onValueChanged}
+                value={onValueChanged == undefined ? formatUnits(value ?? 0n, tokenData?.decimals ?? 18): undefined}
+                className={clsxm('text-right w-full pl-12 text-xs py-2', !isValidAmount && 'text-red-500')}
+                onChange={(e) => {
+                    const value = e.target.value;
+                    if (!isNaN(Number(value))) {
+                        const val = parseUnits(value == '' ? '0' : value as `${number}`, tokenData?.decimals ?? 18);
+                        onValueChanged?.(val);
+                    } else {
+                        onValueChanged?.(BigInt(-1));
+                    }
+                }}/>
+        </div>
+
+
+    </div>
+}
+
+export function TokenSelectorDialog({isOpen, setIsOpen, onAdd}: {
+    isOpen: boolean,
+    setIsOpen: (isOpen: boolean) => void,
+    onAdd: (address: Address, name: string) => void
+}) {
+    return <Modal render={closeModal => <TokenSelectorFrom onAdd={onAdd} closeModal={closeModal}/>} isOpen={isOpen}
+                  setIsOpen={setIsOpen}/>
+}
+
+export function TokenSelectorFrom({closeModal, onAdd}: {
+    closeModal: () => void,
+    onAdd: (address: Address, name: string) => void
+}) {
+    const [selected, setSelected] = useState('')
+    const [query, setQuery] = useState('')
+    const addresses = usePackdAddresses();
+    const data = useMemo(() => [
+        {name: 'MKT A', address: addresses?.ERC20MockA},
+        {name: 'MKT B', address: addresses?.ERC20MockB},
+    ], [addresses?.PackMain])
+
+    const [filteredTokens, setFilteredTokens] = useState(data)
+
+
+    useEffect(() => {
+        setFilteredTokens(query === ''
+            ? data
+            : data.filter((item) =>
+                item.name
+                    .toLowerCase()
+                    .replace(/\s+/g, '')
+                    .includes(query.toLowerCase().replace(/\s+/g, '')) || item.address
+                    .toLowerCase()
+                    .replace(/\s+/g, '')
+                    .includes(query.toLowerCase().replace(/\s+/g, ''))
+            ))
+    }, [query, data]);
+
+    useEffect(() => {
+        console.log(isAddress(query), filteredTokens.length)
+        if (isAddress(query) && filteredTokens.length === 0) {
+            setFilteredTokens([{name: 'Unknown', address: query}])
+        }
+    }, [query, filteredTokens]);
+
+    return (
+        <div className='flex flex-col bg-gray-900 rounded-3xl gap-2 text-white p-4'>
+            <div className="flex justify-between">
+                <span className='text-card-title'>Select token</span>
+                <button className="hover:text-primary-500 pl-2" onClick={closeModal}><BsX/></button>
+            </div>
+            <div className="border-gray-500 border-b-[1px]">{''}</div>
+            <div className='relative'>
+                <input
+                    placeholder="Search name or paste address  "
+                    className="w-full pl-8 text-xs py-2"
+                    onChange={(event) => setQuery(event.target.value)}
+                />
+                <BsSearch className='absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400'/>
+            </div>
+            <div className="border-gray-500 border-b-[1px]">{''}</div>
+            <div className="flex flex-col gap-2 h-60">
+                {filteredTokens.map((item) => (
+                    <TokenSearchItem onClick={() => onAdd(item.address, item.name)} key={item.address}
+                                     tokenAddress={item.address} name={item.name}/>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+function TokenSearchItem({tokenAddress, name, onClick}: { tokenAddress: string, name: string, onClick: () => void }) {
+    const {address} = useAccount()
+    const {data: balance, isLoading} = useBalance({
+        address: address as Address,
+        token: tokenAddress as Address,
+        enabled: !!address
+    })
+    const {data} = useToken({address: tokenAddress as Address})
+    return <button
+        onClick={onClick}
+        className=" text-left flex items-center p-1 hover:border-gray-500 p rounded-lg border border-transparent">
+        <Icon className='h-6 mr-2'/>
+        <div className='flex flex-col grow'>
+            <span className='text-card-title'>{data?.name}: {data?.symbol}</span>
+            <span className='text-xs text-gray-400'>{tokenAddress}</span>
+        </div>
+        <span className='text-card-title'>{balance?.formatted}</span>
+    </button>
+}
--- packages/app/app/mint/pack/AddTokenModal.tsx
+++ packages/app/app/mint/pack/AddTokenModal.tsx
@@ -5,16 +5,13 @@ import {Card} from "@/app/components/Card";
 import Button from "@/app/components/button/Button";
 import {BsArrowLeft, BsArrowRight} from "react-icons/bs";
 import {ContentCard} from "@/app/components/content/ContentCard";
-import {isAddress} from "viem";
-import clsxm from "@/src/lib/clsxm";
-import {parseEther, parseUnits} from "ethers";
+import TokenInput from "@/app/components/web3/TokenSelectorDialog";
 
 export default function AddTokenModal({isOpen, setIsOpen, onAdd}: { isOpen: boolean, setIsOpen: (isOpen: boolean) => void, onAdd: (address: Address, amount: bigint) => void }) {
     const [tokenAddress, setTokenAddress] = useState<Address>();
     const [amount, setAmount] = useState<bigint>()
     const {address} = useAccount();
     const {isLoading, data} = useBalance({address, token: tokenAddress, enabled: !!tokenAddress});
-    const {data: tokenData} = useToken({address: tokenAddress})
     const isValidAmount = amount && amount > 0 && (amount < (data?.value ?? BigInt(0)));
     const add = useCallback(() => {
         if (!tokenAddress || !isValidAmount) return;
@@ -42,35 +39,19 @@ export default function AddTokenModal({isOpen, setIsOpen, onAdd}: { isOpen: bool
             <div className="flex flex-col gap-2">
                 <ContentCard className='self-stretch'>
                     <div className="flex justify-between">
-                        <span className='text-card-title'>Token address</span>
+                        <span className='text-card-title'>Token</span>
                         {tokenAddress && <span className='text-card-title'>Available: {data?.formatted}</span>}
                     </div>
-                    <div className="relative">
-                        {tokenData && <div className="absolute left-0 top-0 bottom-0 flex items-center pl-2">
-                            {tokenData?.symbol}
-                        </div>}
-                        <input className='text-right w-full pl-12 text-xs py-2' onChange={(e) => {
-                            const value = e.target.value;
-                            if (isAddress(value)) {
-                                setTokenAddress(value as Address);
-                            } else {
-                                setTokenAddress(undefined)
-                            }
-                        }}/>
-                    </div>
 
-                    <span className='text-card-title'>Amount</span>
-                    <input className={clsxm('text-right', !isValidAmount && 'text-red-500')}
-                           disabled={!tokenData}
-                           onChange={(e) => {
-                               const value = e.target.value;
-                               if (!isNaN(Number(value))) {
-                                   const val = parseUnits(value == '' ? '0' : value as `${number}`, tokenData?.decimals ?? 18);
-                                   setAmount(val);
-                               } else {
-                                   setAmount(BigInt(-1));
-                               }
-                           }}/>
+                        <TokenInput
+                        onTokenSelected={setTokenAddress}
+                        token={tokenAddress}
+                        onValueChanged={setAmount}
+                        value={amount}
+                        />
+
+
+
                 </ContentCard>
 
             </div>
