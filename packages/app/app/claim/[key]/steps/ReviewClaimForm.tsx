6df16c7c0fc156f42220c9c2d0545b49ad397367
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-10-26 23:40:14 +0700

    feat: "gas Estimates"

--- packages/app/app/claim/[key]/steps/ReviewClaimForm.tsx
+++ packages/app/app/claim/[key]/steps/ReviewClaimForm.tsx
@@ -1,144 +1,163 @@
-import {useCallback, useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 import Button from "@/app/components/button/Button";
-import {FiArrowRight} from "react-icons/fi";
-import {useClaimState} from "@/app/claim/[key]/useClaimState";
-import {useConnectModal} from "@rainbow-me/rainbowkit";
-import {Address, useAccount, useNetwork} from "wagmi";
+import { FiArrowRight } from "react-icons/fi";
+import { useClaimState } from "@/app/claim/[key]/useClaimState";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { Address, useAccount, useNetwork } from "wagmi";
 import StepperIndicator from "@/app/claim/[key]/steps/components/StepperIndicator";
-import Arrow from '~/arrow.svg'
-import {usePackDataByTokenId} from "@/src/hooks/usePackDataByTokenId";
-import {ReviewData} from "@/app/mint/pack/ReviewForm";
-import {ContentTitle} from "@/app/components/content/ContentRow";
-import {useClaim} from "@/src/hooks/useClaim";
-import {useGenerateClaimData} from "@/src/hooks/useGenerateClaimData";
+import Arrow from "~/arrow.svg";
+import { usePackDataByTokenId } from "@/src/hooks/usePackDataByTokenId";
+import { ReviewData } from "@/app/mint/pack/ReviewForm";
+import { ContentTitle } from "@/app/components/content/ContentRow";
+import { useClaim } from "@/src/hooks/useClaim";
+import { useGenerateClaimData } from "@/src/hooks/useGenerateClaimData";
 import usePackdAddresses from "@/src/hooks/usePackdAddresses";
 import useEnsOrFormattedAddress from "@/src/hooks/useEnsOrAddress";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 // @ts-ignore
 BigInt.prototype.toJSON = function () {
-    return this.toString()
-}
+  return this.toString();
+};
 
-function getErrorMessage(error: string){
-    switch (error) {
-        case "INVALID_BODY_PARAMETERS":
-            return 'Invalid body parameters'
-        case "GAS_ESTIMATATION_FAILED":
-            return 'Gas estimation failed'
-        case 'MAX_REFUND_VALUE_TOO_LOW':
-            return 'Max refund value too low'
-        case "UNABLE_TO_BROADCAST_TX":
-            return 'Unable to broadcast transaction'
-    }
-    return 'Unexpected Error'
-
+function getErrorMessage(error: string) {
+  switch (error) {
+    case "INVALID_BODY_PARAMETERS":
+      return "Invalid body parameters";
+    case "GAS_ESTIMATATION_FAILED":
+      return "Gas estimation failed";
+    case "MAX_REFUND_VALUE_TOO_LOW":
+      return "Max refund value too low";
+    case "UNABLE_TO_BROADCAST_TX":
+      return "Unable to broadcast transaction";
+  }
+  return "Unexpected Error";
 }
 export default function ReviewClaimForm() {
-    const nextStep = useClaimState(state => state.nextStep)
-    const previousStep = useClaimState(state => state.previousStep)
-    const setControls = useClaimState(state => state.setControls)
-    const owner = useClaimState(state => state.owner)
-    const addresses = usePackdAddresses();
-    const {openConnectModal} = useConnectModal()
-    const {address} = useAccount()
-    const {chain} = useNetwork()
-    const tokenId = useClaimState(state => state.mintedTokenId);
-    const {packData, rawEth, isLoading: isTokenDataLoading} = usePackDataByTokenId(tokenId!);
-    const setLoading = useClaimState(state => state.setLoading);
-    const setSendingToRelayer = useClaimState(state => state.setSendingToRelayer);
+  const nextStep = useClaimState((state) => state.nextStep);
+  const previousStep = useClaimState((state) => state.previousStep);
+  const setControls = useClaimState((state) => state.setControls);
+  const owner = useClaimState((state) => state.owner);
+  const addresses = usePackdAddresses();
+  const { openConnectModal } = useConnectModal();
+  const { address } = useAccount();
+  const { chain } = useNetwork();
+  const tokenId = useClaimState((state) => state.mintedTokenId);
+  const {
+    packData,
+    rawEth,
+    isLoading: isTokenDataLoading,
+  } = usePackDataByTokenId(tokenId!);
+  const setLoading = useClaimState((state) => state.setLoading);
+  const setSendingToRelayer = useClaimState(
+    (state) => state.setSendingToRelayer
+  );
 
-    const maxRefundValue = useClaimState(state => state.maxRefundValue);
-    const signedData = useClaimState(state => state.signedMessage);
-    const privateKey = useClaimState(state => state.privateKey);
-    const claimData = useGenerateClaimData(
-        address!,
-        maxRefundValue,
-        signedData!,
-        tokenId!,
-        privateKey!
+  const maxRefundValue = useClaimState((state) => state.maxRefundValue);
+  const signedData = useClaimState((state) => state.signedMessage);
+  const privateKey = useClaimState((state) => state.privateKey);
+  const claimData = useGenerateClaimData(
+    address!,
+    maxRefundValue,
+    signedData!,
+    tokenId!,
+    privateKey!
+  );
+
+  const { write, data, isLoading } = useClaim(
+    claimData,
+    isTokenDataLoading ? undefined : packData?.moduleData ?? []
+  );
+
+  const writeToRelayer = useCallback(async () => {
+    setSendingToRelayer(true);
+    const body = {
+      mainContractAddress: addresses.PackMain,
+      chainId: chain?.id,
+      args: claimData,
+      moduleData: packData?.moduleData ?? [],
+    };
+    const res = await fetch("/api/claim", {
+      body: JSON.stringify(body),
+      method: "POST",
+    });
+    if (res.ok) {
+      const data = await res.json();
+      setLoading(data.hash);
+    } else {
+      const data = await res.json();
+      toast.error(getErrorMessage(data.error), { autoClose: 2000 });
+    }
+    setSendingToRelayer(false);
+  }, [addresses.PackMain, claimData, setLoading]);
+
+  useEffect(() => {
+    if (data?.hash) {
+      setLoading(data!.hash);
+    }
+  }, [data, data?.hash, setLoading, setSendingToRelayer]);
+
+  useEffect(() => {
+    setControls(
+      <div className="w-full flex justify-between py-1 items-center">
+        <StepperIndicator step={2} />
+        <Button
+          isLoading={isLoading}
+          onClick={() => writeToRelayer()}
+          variant="navigation"
+          rightIcon={<FiArrowRight className="text-inherit inline" />}
+        >
+          Confirm Claim
+        </Button>
+      </div>
     );
+  }, [
+    writeToRelayer,
+    nextStep,
+    setControls,
+    previousStep,
+    address,
+    openConnectModal,
+    isLoading,
+  ]);
 
-    const {
-        write,
-        data,
-        isLoading,
-    } = useClaim(claimData, isTokenDataLoading ? undefined : (packData?.moduleData ?? []));
-
-    const writeToRelayer = useCallback(async () => {
-        setSendingToRelayer(true);
-        const body = {
-            mainContractAddress: addresses.PackMain,
-            chainId: chain?.id,
-            args: claimData,
-            moduleData: packData?.moduleData ?? []
-        }
-        const res = await fetch('/api/claim', {body: JSON.stringify(body), method: 'POST'});
-        if (res.ok) {
-            const data = await res.json();
-            setLoading(data.hash);
-        } else {
-            const data = await res.json();
-            toast.error(getErrorMessage(data.error), {autoClose: 2000})
-        }
-        setSendingToRelayer(false);
-
-    }, [addresses.PackMain, claimData, setLoading]);
-
-    useEffect(() => {
-            if (data?.hash) {
-                setLoading(data!.hash)
-            }
-        }, [data, data?.hash, setLoading, setSendingToRelayer]
-    )
-
-
-    useEffect(() => {
-        setControls(<div className='w-full flex justify-between py-1 items-center'>
-            <StepperIndicator step={2}/>
-            <Button
-                isLoading={isLoading}
-                onClick={() => writeToRelayer()}
-                variant="navigation" rightIcon={<FiArrowRight className='text-inherit inline'/>}>
-                Confirm Claim
-            </Button>
-
-        </div>)
-    }, [writeToRelayer, nextStep, setControls, previousStep, address, openConnectModal, isLoading]);
-
-    const ownerName = useEnsOrFormattedAddress(owner as Address);
-    const claimerName = useEnsOrFormattedAddress(address);
-    return <div className="flex flex-col w-full gap-2 items-stretch">
-        <div className='flex p-2 rounded-full bg-gray-800 items-center justify-around gap-4'>
-            <div className="p-2 text-sm">
-                <div className='text-gray-400'>From</div>
-                {ownerName}
-            </div>
-            <Arrow className="h-12 w-8"/>
-            <div className="p-2 text-sm">
-                <div className="text-right text-gray-400">To</div>
-                {claimerName}
-            </div>
+  const ownerName = useEnsOrFormattedAddress(owner as Address);
+  const claimerName = useEnsOrFormattedAddress(address);
+  return (
+    <div className="flex flex-col w-full gap-2 items-stretch">
+      <div className="flex p-2 rounded-full bg-gray-800 items-center justify-around gap-4">
+        <div className="p-2 text-sm">
+          <div className="text-gray-400">From</div>
+          {ownerName}
         </div>
-        <ContentTitle>Contents</ContentTitle>
-
-        <div className="flex flex-col gap-2">
-            {<ReviewData eth={rawEth ?? BigInt(0)}
-                         modules={packData?.fullModuleData ?? []}/>}
+        <Arrow className="h-12 w-8" />
+        <div className="p-2 text-sm">
+          <div className="text-right text-gray-400">To</div>
+          {claimerName}
         </div>
-        <table className="font-semibold mt-4">
-            <tbody>
-            <tr>
-                <td className='text-gray-500'>Chain</td>
-                <td className='text-right'>{chain?.name}</td>
-            </tr>
-            <tr>
-                <td className='text-gray-500'>Gas fees</td>
-                <td className='text-right'>$1.00</td>
-            </tr>
-            </tbody>
-        </table>
+      </div>
+      <ContentTitle>Contents</ContentTitle>
 
+      <div className="flex flex-col gap-2">
+        {
+          <ReviewData
+            eth={rawEth ?? BigInt(0)}
+            modules={packData?.fullModuleData ?? []}
+          />
+        }
+      </div>
+      <table className="font-semibold mt-4">
+        <tbody>
+          <tr>
+            <td className="text-gray-500">Chain</td>
+            <td className="text-right">{chain?.name}</td>
+          </tr>
+          <tr>
+            <td className="text-gray-500">Gas fees</td>
+            <td className="text-right">~0.000493 ETH</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-
+  );
 }
--- packages/app/app/mint/pack/ReviewForm.tsx
+++ packages/app/app/mint/pack/ReviewForm.tsx
@@ -1,95 +1,111 @@
-import {ContentTitle} from "@/app/components/content/ContentRow";
-import {useEffect} from "react";
+import { ContentTitle } from "@/app/components/content/ContentRow";
+import { useEffect } from "react";
 import Button from "@/app/components/button/Button";
-import {FiArrowLeft, FiArrowRight} from "react-icons/fi";
-import {usePackState} from "@/app/mint/usePackState";
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
+import { usePackState } from "@/app/mint/usePackState";
 import usePackdAddresses from "@/src/hooks/usePackdAddresses";
-import {useNetwork} from "wagmi";
-import {useMintStore, Module} from "@/src/stores/useMintStore";
+import { useNetwork } from "wagmi";
+import { useMintStore, Module } from "@/src/stores/useMintStore";
 import Erc721Card from "@/app/mint/modules/Erc721Module";
 import Erc20Card from "@/app/mint/modules/Erc20Module";
-import {ContentCard} from "@/app/components/content/ContentCard";
+import { ContentCard } from "@/app/components/content/ContentCard";
 import useMintPack from "@/src/hooks/useMintPack";
-import {formatEther} from "ethers";
-
+import { formatEther } from "ethers";
 
 export const ReviewForm = () => {
-
-    const setHash = usePackState(state => state.setLoading)
-    const previousStep = usePackState(state => state.previousStep)
-    const setControls = usePackState(state => state.setControls)
-    const addresses = usePackdAddresses();
-    const modules = useMintStore(state => state.modules)
-    const amountEth = useMintStore(state => state.eth)
-    const {chain} = useNetwork();
-    const {write, isLoading, error, data} = useMintPack();
-    useEffect(() => {
-        if (data?.hash) {
-            setHash(data!.hash)
-        }
-    }, [data, setHash]);
-    useEffect(() => {
-        setControls(<div className='w-full flex justify-between py-1'>
-            <Button
-                onClick={previousStep}
-                variant="navigation"
-                leftIcon={<FiArrowLeft className='text-inherit inline'/>}>
-                Back
-            </Button>
-            <Button
-                onClick={() => write && write()}
-                variant="navigation" rightIcon={<FiArrowRight className='text-inherit inline'/>}>
-                Pack it!
-            </Button>
-        </div>)
-    }, [setControls, previousStep, write]);
-    return (
-        <div className="flex flex-col w-full gap-2">
-            <div className='text-center pb-8'>
-                <h2 className="text-2xl font-bold ">Review Pack Content</h2>
-            </div>
-            <ContentTitle>Contents</ContentTitle>
-            <ReviewData
-            eth={amountEth}
-            modules={modules}/>
-            <table className="font-semibold mt-4">
-                <tbody>
-                <tr>
-                    <td className='text-gray-500'>Chain</td>
-                    <td className='text-right'>{chain?.name}</td>
-                </tr>
-                <tr>
-                    <td className='text-gray-500'>Gas fees</td>
-                    <td className='text-right'>$1.00</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>);
+  const setHash = usePackState((state) => state.setLoading);
+  const previousStep = usePackState((state) => state.previousStep);
+  const setControls = usePackState((state) => state.setControls);
+  const addresses = usePackdAddresses();
+  const modules = useMintStore((state) => state.modules);
+  const amountEth = useMintStore((state) => state.eth);
+  const { chain } = useNetwork();
+  const { write, isLoading, error, data } = useMintPack();
+  useEffect(() => {
+    if (data?.hash) {
+      setHash(data!.hash);
+    }
+  }, [data, setHash]);
+  useEffect(() => {
+    setControls(
+      <div className="w-full flex justify-between py-1">
+        <Button
+          onClick={previousStep}
+          variant="navigation"
+          leftIcon={<FiArrowLeft className="text-inherit inline" />}
+        >
+          Back
+        </Button>
+        <Button
+          onClick={() => write && write()}
+          variant="navigation"
+          rightIcon={<FiArrowRight className="text-inherit inline" />}
+        >
+          Pack it!
+        </Button>
+      </div>
+    );
+  }, [setControls, previousStep, write]);
+  return (
+    <div className="flex flex-col w-full gap-2">
+      <div className="text-center pb-8">
+        <h2 className="text-2xl font-bold ">Review Pack Content</h2>
+      </div>
+      <ContentTitle>Contents</ContentTitle>
+      <ReviewData eth={amountEth} modules={modules} />
+      <table className="font-semibold mt-4">
+        <tbody>
+          <tr>
+            <td className="text-gray-500">Chain</td>
+            <td className="text-right">{chain?.name}</td>
+          </tr>
+          <tr>
+            <td className="text-gray-500">Gas fees</td>
+            <td className="text-right">0 ETH (Pack-Funded)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export function ReviewData({eth, modules}: { eth: bigint, modules: Module[] }) {
-    const addresses = usePackdAddresses();
-    return <>
-        <ContentCard className="self-stretch">
-            <div className="flex justify-between">
-                <span className="text-card-title">ETH</span>
-            </div>
-            <input className="text-right w-full " disabled={true}
-                   value={formatEther(eth ?? 0)}/>
-        </ContentCard>
-        {modules.map((module, index) => {
-            if (module.moduleAddress === addresses.ERC721Module) {
-                return <Erc721Card key={module.address + module.value}
-                                   module={module}/>
-            }
-            if (module.moduleAddress === addresses.ERC20Module) {
-                return <Erc20Card key={module.address + module.value}
-                                  module={module}/>
-            }
-            return <ContentCard key={module.address + module.value}>
-                <ContentTitle>Unknown module</ContentTitle>
-            </ContentCard>;
-        })
+export function ReviewData({
+  eth,
+  modules,
+}: {
+  eth: bigint;
+  modules: Module[];
+}) {
+  const addresses = usePackdAddresses();
+  return (
+    <>
+      <ContentCard className="self-stretch">
+        <div className="flex justify-between">
+          <span className="text-card-title">ETH</span>
+        </div>
+        <input
+          className="text-right w-full "
+          disabled={true}
+          value={formatEther(eth ?? 0)}
+        />
+      </ContentCard>
+      {modules.map((module, index) => {
+        if (module.moduleAddress === addresses.ERC721Module) {
+          return (
+            <Erc721Card key={module.address + module.value} module={module} />
+          );
         }
+        if (module.moduleAddress === addresses.ERC20Module) {
+          return (
+            <Erc20Card key={module.address + module.value} module={module} />
+          );
+        }
+        return (
+          <ContentCard key={module.address + module.value}>
+            <ContentTitle>Unknown module</ContentTitle>
+          </ContentCard>
+        );
+      })}
     </>
+  );
 }
