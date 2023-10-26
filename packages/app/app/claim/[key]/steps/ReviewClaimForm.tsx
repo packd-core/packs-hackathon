bea7f919043cb99fee5333bd2174dc60e4bb881c
kodam <kolozsvariadam@gmail.com>
2023-10-26 18:01:39 +0200

    feat: add error messages to claim api call

--- packages/app/app/claim/[key]/steps/ReviewClaimForm.tsx
+++ packages/app/app/claim/[key]/steps/ReviewClaimForm.tsx
@@ -1,32 +1,50 @@
 import {useCallback, useEffect, useState} from "react";
 import Button from "@/app/components/button/Button";
-import { FiArrowRight } from "react-icons/fi";
-import { useClaimState } from "@/app/claim/[key]/useClaimState";
-import { useConnectModal } from "@rainbow-me/rainbowkit";
-import { Address, useAccount, useNetwork } from "wagmi";
+import {FiArrowRight} from "react-icons/fi";
+import {useClaimState} from "@/app/claim/[key]/useClaimState";
+import {useConnectModal} from "@rainbow-me/rainbowkit";
+import {Address, useAccount, useNetwork} from "wagmi";
 import StepperIndicator from "@/app/claim/[key]/steps/components/StepperIndicator";
 import Arrow from '~/arrow.svg'
-import { usePackDataByTokenId } from "@/src/hooks/usePackDataByTokenId";
-import { ReviewData } from "@/app/mint/pack/ReviewForm";
-import { ContentTitle } from "@/app/components/content/ContentRow";
-import { useClaim } from "@/src/hooks/useClaim";
-import { useGenerateClaimData } from "@/src/hooks/useGenerateClaimData";
+import {usePackDataByTokenId} from "@/src/hooks/usePackDataByTokenId";
+import {ReviewData} from "@/app/mint/pack/ReviewForm";
+import {ContentTitle} from "@/app/components/content/ContentRow";
+import {useClaim} from "@/src/hooks/useClaim";
+import {useGenerateClaimData} from "@/src/hooks/useGenerateClaimData";
 import usePackdAddresses from "@/src/hooks/usePackdAddresses";
 import useEnsOrFormattedAddress from "@/src/hooks/useEnsOrAddress";
+import {toast} from "react-toastify";
 
 // @ts-ignore
-BigInt.prototype.toJSON = function () { return this.toString() }
+BigInt.prototype.toJSON = function () {
+    return this.toString()
+}
+
+function getErrorMessage(error: string){
+    switch (error) {
+        case "INVALID_BODY_PARAMETERS":
+            return 'Invalid body parameters'
+        case "GAS_ESTIMATATION_FAILED":
+            return 'Gas estimation failed'
+        case 'MAX_REFUND_VALUE_TOO_LOW':
+            return 'Max refund value too low'
+        case "UNABLE_TO_BROADCAST_TX":
+            return 'Unable to broadcast transaction'
+    }
+    return 'Unexpected Error'
+
+}
 export default function ReviewClaimForm() {
     const nextStep = useClaimState(state => state.nextStep)
     const previousStep = useClaimState(state => state.previousStep)
     const setControls = useClaimState(state => state.setControls)
     const owner = useClaimState(state => state.owner)
     const addresses = usePackdAddresses();
-    const { openConnectModal } = useConnectModal()
-    const { address } = useAccount()
-    const { chain } = useNetwork()
+    const {openConnectModal} = useConnectModal()
+    const {address} = useAccount()
+    const {chain} = useNetwork()
     const tokenId = useClaimState(state => state.mintedTokenId);
-    const { packData, rawEth, isLoading: isTokenDataLoading } = usePackDataByTokenId(tokenId!);
+    const {packData, rawEth, isLoading: isTokenDataLoading} = usePackDataByTokenId(tokenId!);
     const setLoading = useClaimState(state => state.setLoading);
     const setSendingToRelayer = useClaimState(state => state.setSendingToRelayer);
 
@@ -55,30 +73,33 @@ export default function ReviewClaimForm() {
             args: claimData,
             moduleData: packData?.moduleData ?? []
         }
-        const res = await fetch('/api/claim', { body: JSON.stringify(body), method: 'POST' });
+        const res = await fetch('/api/claim', {body: JSON.stringify(body), method: 'POST'});
         if (res.ok) {
             const data = await res.json();
             setLoading(data.hash);
+        } else {
+            const data = await res.json();
+            toast.error(getErrorMessage(data.error), {autoClose: 2000})
         }
         setSendingToRelayer(false);
 
     }, [addresses.PackMain, claimData, setLoading]);
 
     useEffect(() => {
-        if (data?.hash) {
-            setLoading(data!.hash)
-        }
-    }, [data, data?.hash, setLoading, setSendingToRelayer]
+            if (data?.hash) {
+                setLoading(data!.hash)
+            }
+        }, [data, data?.hash, setLoading, setSendingToRelayer]
     )
 
 
     useEffect(() => {
         setControls(<div className='w-full flex justify-between py-1 items-center'>
-            <StepperIndicator step={2} />
+            <StepperIndicator step={2}/>
             <Button
                 isLoading={isLoading}
                 onClick={() => writeToRelayer()}
-                variant="navigation" rightIcon={<FiArrowRight className='text-inherit inline' />}>
+                variant="navigation" rightIcon={<FiArrowRight className='text-inherit inline'/>}>
                 Confirm Claim
             </Button>
 
@@ -93,7 +114,7 @@ export default function ReviewClaimForm() {
                 <div className='text-gray-400'>From</div>
                 {ownerName}
             </div>
-            <Arrow className="h-12 w-8" />
+            <Arrow className="h-12 w-8"/>
             <div className="p-2 text-sm">
                 <div className="text-right text-gray-400">To</div>
                 {claimerName}
@@ -107,14 +128,14 @@ export default function ReviewClaimForm() {
         </div>
         <table className="font-semibold mt-4">
             <tbody>
-                <tr>
-                    <td className='text-gray-500'>Chain</td>
-                    <td className='text-right'>{chain?.name}</td>
-                </tr>
-                <tr>
-                    <td className='text-gray-500'>Gas fees</td>
-                    <td className='text-right'>$1.00</td>
-                </tr>
+            <tr>
+                <td className='text-gray-500'>Chain</td>
+                <td className='text-right'>{chain?.name}</td>
+            </tr>
+            <tr>
+                <td className='text-gray-500'>Gas fees</td>
+                <td className='text-right'>$1.00</td>
+            </tr>
             </tbody>
         </table>
 
